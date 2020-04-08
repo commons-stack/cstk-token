45f037ccb8b74924a2e05a039e28ea2e0bb92fd2
Nelson <nelsonmelina@pm.me>
2020-04-08 23:07:27 +0200

    Add error message, and revert if address(0) is used in array.

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -6,7 +6,7 @@ import "./RegistryAbstract.sol";
 contract Registry is RegistryAbstract {
     mapping(address => ContributorInfo) contributors;
 
-    constructor(address[] memory _admins) RegistryAbstract(_admins) public {}
+    constructor(address[] memory _admins) public RegistryAbstract(_admins) {}
 
     function registerContributors(
         address[] memory wallets,
@@ -19,17 +19,19 @@ contract Registry is RegistryAbstract {
         address[] memory wallets,
         uint256[] memory allowed
     ) internal {
-        require(wallets.length == allowed.length);
+        require(
+            wallets.length == allowed.length,
+            "wallets and allowed values need to be the same length"
+        );
         for (uint256 i = 0; i < wallets.length; ++i) {
-            if (wallets[i] != address(0)) {
-                ContributorInfo memory newContributor = ContributorInfo(
-                    wallets[i],
-                    allowed[i],
-                    true
-                );
-                contributors[newContributor.wallet] = newContributor;
-                emit ContributorAdded(newContributor.wallet);
-            }
+            require(wallets[i] != address(0), "address can not be address(0)");
+            ContributorInfo memory newContributor = ContributorInfo(
+                wallets[i],
+                allowed[i],
+                true
+            );
+            contributors[newContributor.wallet] = newContributor;
+            emit ContributorAdded(newContributor.wallet);
         }
     }
 
@@ -39,13 +41,12 @@ contract Registry is RegistryAbstract {
 
     function _removeContributors(address[] memory wallets) internal {
         for (uint256 i = 0; i < wallets.length; ++i) {
-            if (wallets[i] != address(0)) {
-                delete contributors[wallets[i]].wallet;
-                delete contributors[wallets[i]].allowed;
-                delete contributors[wallets[i]].active;
-                delete contributors[wallets[i]];
-                emit ContributorRemoved(wallets[i]);
-            }
+            require(wallets[i] != address(0), "address can not be address(0)");
+            delete contributors[wallets[i]].wallet;
+            delete contributors[wallets[i]].allowed;
+            delete contributors[wallets[i]].active;
+            delete contributors[wallets[i]];
+            emit ContributorRemoved(wallets[i]);
         }
     }
 
