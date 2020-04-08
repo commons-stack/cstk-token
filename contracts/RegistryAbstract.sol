1bad7b417cad533a2acdc4315b5d9b64a3669297
Nelson <nelsonmelina@pm.me>
2020-04-08 19:59:50 +0200

    Added interface for whitelist and admin management

---
+++ contracts/RegistryAbstract.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+import "./AdminRole.sol";
+
+
+contract RegistryAbstract is AdminRole {
+
+    constructor(address[] memory _admins) internal AdminRole(_admins) {}
+
+    event ContributorAdded(address wallet);
+    event ContributorRemoved(address wallet);
+
+    struct ContributorInfo {
+        address wallet;
+        uint256 allowed;
+        bool active;
+    }
+
+    function registerContributors(address[] memory wallets) public;
+
+    function removeContributors(address[] memory wallets) public;
+
+    function getAllowed(address wallet) public view returns (uint256 allowed);
+
+    function isContributor(address wallet) public view returns (bool);
+}
