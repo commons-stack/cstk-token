c9c58cbe52fd2e9c7309f67e5dfe505b16d7195c
Pavle Batuta <pavle@batuta.xyz>
2020-04-09 14:43:41 +0200

    Add dotenv based config file
    Config file is based on the `.env.example` template. Contributors are
    expected to modify this template to their local `.env`.
    
    Updated README.md to eplain how to install and test.

--- truffle-config.js
+++ truffle-config.js
@@ -1,33 +1,6 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+require("dotenv").config();
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
-
-// var PrivateKeyProvider = require("truffle-privatekey-provider");
-// var privateKey = "62537136911bca3a7e2b....";
-// var pkProvider = new PrivateKeyProvider(privateKey, "https://ropsten.infura.io/U8U4n8mm2wDgB2e3Dksv ");
-
 
 module.exports = {
     /**
@@ -54,8 +27,8 @@ module.exports = {
         },
 
         ropsten: {
-            provider: () => new HDWalletProvider(mnemonic,
-                `https://ropsten.infura.io/v3/6a3f60b916694db9a87d4f8296a5299d`,
+            provider: () => new HDWalletProvider(process.env.MNEMONIC,
+                `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
                 0,
                 10),
             network_id: 3,       // Ropsten's id
@@ -98,18 +71,18 @@ module.exports = {
         // timeout: 100000
     },
 
-    // Configure your compilers
+    // Configure your compilers:
     compilers: {
         solc: {
-            // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+            version: "0.5.16",      // Fetch exact version from solc-bin (default: truffle's version)
             // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-            // settings: {          // See the solidity docs for advice about optimization and evmVersion
-            //  optimizer: {
-            //    enabled: false,
-            //    runs: 200
-            //  },
-            //  evmVersion: "byzantium"
-            // }
+            settings: {
+                optimizer: {
+                    enabled: false,
+                    runs: 200
+                },
+                emvVersion: "byzantium"
+            }
         }
     },
 }
