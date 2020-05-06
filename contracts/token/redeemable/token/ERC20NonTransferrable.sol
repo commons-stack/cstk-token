442f794c0740fda5927e9a262cd8e4c9880351bc
Pavle Batuta <pavle@batuta.xyz>
2020-05-06 19:04:14 +0200

    Replace require(false) with revert

--- contracts/token/redeemable/token/ERC20NonTransferrable.sol
+++ contracts/token/redeemable/token/ERC20NonTransferrable.sol
@@ -5,11 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20NonTransferrable is ERC20 {
     function transfer(address, uint256) public returns (bool) {
-        require(false, "Token non transferrable");
+        revert("Token non transferrable");
     }
 
     function approve(address, uint256) public returns (bool) {
-        require(false, "Token non transferrable");
+        revert("Token non transferrable");
     }
 
     function transferFrom(
@@ -17,14 +17,14 @@ contract ERC20NonTransferrable is ERC20 {
         address,
         uint256
     ) public returns (bool) {
-        require(false, "Token non transferrable");
+        revert("Token non transferrable");
     }
 
     function increaseAllowance(address, uint256) public returns (bool) {
-        require(false, "Token non transferrable");
+        revert("Token non transferrable");
     }
 
     function decreaseAllowance(address, uint256) public returns (bool) {
-        require(false, "Token non transferrable");
+        revert("Token non transferrable");
     }
 }
