b5b0ef19f9015bddf0d4b52193dc3b2db56e6012
Pavle Batuta <pavle@batuta.xyz>
2020-10-03 16:54:30 +0200

    Add initial minter contract

---
+++ contracts/Minter.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.5.0;
+
+import "./registry/Registry.sol";
+import "./registry/AdminRole.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract Minter {
+    using SafeMath for uint256;
+
+    event Donate(
+        address indexed sender,
+        address indexed token,
+        uint64 indexed receiverId,
+        uint256 amount,
+        uint256 receivedCSTK,
+        bytes32 homeTx
+    );
+
+    Registry internal registry;
+    IERC20 internal cstkToken;
+
+    address public authorizedKey;
+
+    uint256 public numerator;
+    uint256 public denominator;
+
+    constructor(
+        address _authorizedKey,
+        address _registryAddress,
+        address _cstkTokenAddress
+    ) public {
+        require(_authorizedKey != address(0), "Authorized key cannot be empty");
+
+        registry = Registry(_registryAddress);
+        cstkToken = IERC20(_cstkTokenAddress);
+
+        authorizedKey = _authorizedKey;
+    }
+
+    modifier onlyAuthorizedKey {
+        require(msg.sender == authorizedKey, "Permission denied");
+        _;
+    }
+
+    function setRatio(uint256 _numerator, uint256 _denominator)
+        external
+        onlyAuthorizedKey
+    {
+        numerator = _numerator;
+        denominator = _denominator;
+    }
+
+    function deposit(
+        address sender,
+        address token,
+        uint64 receiverId,
+        uint256 amount,
+        bytes32 homeTx
+    ) external onlyAuthorizedKey {
+        uint256 amountCSTK = amount.mul(numerator).div(denominator);
+        uint256 totalSupply = cstkToken.totalSupply();
+
+        uint256 maxTrust = registry.getMaxTrust(sender);
+        uint256 senderBalance = cstkToken.balanceOf(sender);
+        require(
+            maxTrust.mul(totalSupply).div(10000000) >=
+                senderBalance + amountCSTK,
+            "not allowed"
+        );
+
+        // TODO: call mint function in DAO
+        // DAOaddress.mint(amountOfCSTK,sender,cstkToken)??
+
+        emit Donate(sender, token, receiverId, amount, amountCSTK, homeTx);
+    }
+}
